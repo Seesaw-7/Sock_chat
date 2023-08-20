f705cb4573d9571633c549a7b0f7f14c0ff3718a
Saw <weiseesaw@gmail.com>
2023-08-20 20:13:15 +0800

    2 procs send and receive

--- client.py
+++ client.py
@@ -1,15 +1,38 @@
-import socket, time
+import os
+import psutil
+import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('localhost', 8888)
 
-client_socket.connect(server_address) # This procedure contains underlying TCP three way handshake
+client_socket.connect(server_address) 
 
-message = 'Hello from the client!'
-client_socket.send(message.encode())
+def child_process():
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            exit()
+        print("Friend said: ",data.decode())
 
-response = client_socket.recv(1024)
-print('Received response from server:', response.decode())
+def main():
+    
+    child_pid = os.fork()
+    
+    if child_pid == 0: # running in the child process.
+        child_process()
+    else: # running in the parent process.
+        while True:
+            msg = input("You said: ")
+            client_socket.send(msg.encode())
+            if msg == "bye":
+                child = psutil.Process(child_pid)
+                child.terminate()
+                break
+        os.wait()  # Wait for the child process to finish.
+
+if __name__ == "__main__":
+    main()
+
 
 client_socket.close()
